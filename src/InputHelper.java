84125d2c1094005f7f88b2568d5bec23797c5921
Emory George <91912952+emorypgeorge@users.noreply.github.com>
2024-10-21 10:38:07 -0400

    Initial commit

---
+++ src/InputHelper.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class InputHelper {
+
+    //run tests of methods here
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+
+    /*
+     * Method ensures that the user inputs a String that is not blank
+     * Method returns the valid input entered by the user
+     */
+    public static String getNonZeroLengthString() {
+        
+    }
+
+    /*
+     * Asks the user if they would like to continue the process
+     * Only accepts Y or N
+     * Invalid input will result in the program asking the user to try again
+     * Returns true if Y is entered
+     * Returns false if N is entered
+     */
+    public static boolean continueYN(){
+
+    }
+
+    /*
+     * Prompts the user to enter a positive non-zero integer
+     * Loops until valid input is received
+     * Returns valid input value
+     */
+    public static int getPositiveNonZeroInt(){
+
+    }
+
+    /*
+     * What does this method do?
+     */
+    public static int getRangedInt(){
+
+    }
+
+    /*
+     * What does this method do?
+     */
+    public static int getInt() {
+
+    }
+
+    /*
+     * What does this method do?
+     */
+    public static double getDouble() {
+
+    }
+
+    /*
+     * What does this method do?
+     */
+    public static double getRangedDouble(){
+
+    }
+
+    /*
+     * What does this method do?
+     */
+    public static String getRegExString() {
+
+    }
+}
